54b9fecbde99578555c34175fa795cd65082a33d
spacebunny <hyperspacebunny@users.noreply.github.com>
2023-03-15 18:27:09 +0200

    Add tests for checkpointing from contracts

--- tests/unitary/OffchainBorrowingGaugeV1/test_checkpoint.py
+++ tests/unitary/OffchainBorrowingGaugeV1/test_checkpoint.py
@@ -16,3 +16,11 @@ def test_user_checkpoint_new_period(accounts, chain, ocb_gauge_v1):
 def test_user_checkpoint_wrong_account(accounts, ocb_gauge_v1):
     with brownie.reverts("dev: unauthorized"):
         ocb_gauge_v1.user_checkpoint(accounts[2], {"from": accounts[1]})
+
+
+def test_user_checkpoint_from_hcontroller(accounts, ocb_gauge_v1, mock_hcontroller):
+    ocb_gauge_v1.user_checkpoint(accounts[1], {"from": mock_hcontroller})
+
+
+def test_user_checkpoint_from_minter(accounts, ocb_gauge_v1, minter):
+    ocb_gauge_v1.user_checkpoint(accounts[1], {"from": minter})
